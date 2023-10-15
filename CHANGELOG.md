fd21c0a203378169e5af479223cadf7755f1e818
Marco Haubrich <marcolino.haubrich@gmail.com>
2023-10-15 17:12:53 -0300

    feat: :sparkles: Exposed all models

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.1.1 (2023-10-15)
+
+- Feature: Add `toMap` and `fromMap` methods to models.
+- Feature: Publicly expose all models.
+
 ## 0.1.0 (2023-10-15)
 
 - Initial version.
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,7 @@
+export 'endereco.dart';
+export 'estabelecimento.dart';
+export 'forma_pagamento.dart';
+export 'item.dart';
 export 'nfe.dart';
+export 'nfe_forma_pagamento.dart';
+export 'nfe_item.dart';
