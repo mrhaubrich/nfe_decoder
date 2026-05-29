a7057d34213152ba2f6989fb8620ac9da51c325b
Marco Aurélio da Rosa Haubrich <marhaubrich@ucs.br>
2026-05-29 14:57:06 -0300

    feat(http-client): ✨ Atualiza testes do cliente HTTP para suportar redirecionamento
    * Refatora os testes para utilizar um cliente HTTP simulado com resposta HTML
    * Adiciona verificação do URL redirecionado e do conteúdo do documento
    * Melhora a legibilidade e a robustez dos testes

--- test/http_client_test.dart
+++ test/http_client_test.dart
@@ -1,11 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:nfe_decoder/decoder/http_client.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final url =
-      "https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?p=43220701132478002858651060002225181164409967|2|1|2|B4C1CE5A575FDEEE31C85F0254A75E98E5E1EEC1";
+  const url =
+      'https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?p=43220701132478002858651060002225181164409967|2|1|2|B4C1CE5A575FDEEE31C85F0254A75E98E5E1EEC1';
+  final redirectedUrl = Uri.parse(
+    'https://dfe-portal.svrs.rs.gov.br/Dfe/QrCodeNFce?p=43220701132478002858651060002225181164409967%7C2%7C1%7C2%7CB4C1CE5A575FDEEE31C85F0254A75E98E5E1EEC1',
+  );
+
   test('test http client redirected URL', () async {
-    HTTPClient httpClient = HTTPClient();
+    final httpClient = HTTPClient(
+      client: _FakeClient(
+        body: '<html><body>NFC-e</body></html>',
+        realUri: redirectedUrl,
+      ),
+    );
     final value = await httpClient.get(url);
 
     expect(value.body, isNotEmpty);
@@ -19,7 +31,19 @@ void main() {
   });
 
   test('test http client document', () async {
-    HTTPClient httpClient = HTTPClient();
+    final httpClient = HTTPClient(
+      client: _FakeClient(
+        body: '''
+          <html>
+            <body>
+              <div>Header</div>
+              <div>DOCUMENTO AUXILIAR DA NOTA FISCAL DE CONSUMIDOR ELETRÔNICA</div>
+            </body>
+          </html>
+        ''',
+        realUri: redirectedUrl,
+      ),
+    );
     final value = await httpClient.fetchDocumentFromLink(url);
 
     expect(value.body?.children, isNotNull);
@@ -32,5 +56,37 @@ void main() {
       value.body?.text,
       contains('DOCUMENTO AUXILIAR DA NOTA FISCAL DE CONSUMIDOR ELETRÔNICA'),
     );
+  });
+}
+
+class _FakeClient extends http.BaseClient {
+  final String body;
+  final Uri realUri;
+
+  _FakeClient({required this.body, required this.realUri});
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    return _FakeStreamedResponse(
+      Stream.value(utf8.encode(body)),
+      200,
+      request: request,
+      url: realUri,
+      headers: {'content-type': 'text/html; charset=utf-8'},
+    );
+  }
+}
+
+class _FakeStreamedResponse extends http.StreamedResponse
+    implements http.BaseResponseWithUrl {
+  @override
+  final Uri url;
+
+  _FakeStreamedResponse(
+    super.stream,
+    super.statusCode, {
+    required this.url,
+    super.request,
+    super.headers,
   });
 }
